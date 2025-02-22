e1fae97f5bde85d2030ca90326f79249ef44c53e
ASperfilev <aleksey.perfilev@ligastavok.ru>
2025-02-22 17:10:20 +0300

    Commit 03

--- ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Не забывайте после правок нажимать \"request review\"");
+        someMethod();
+    }
+
+    private static void someMethod(){
+        System.out.println("t-3 is dune");
     }
 }
