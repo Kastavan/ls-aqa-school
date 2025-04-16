9418700a81b2d5a730d9d8571f75cde098ad8087
Kastavan <kastavan4eg@gmail.com>
2025-04-16 21:59:35 +0300

    Рефакторинг тестов, добавлен тест "Создание нового контакта", добавлен базовый класс TestBase

---
+++ addressbook-web-tests/src/test/java/ru/ls/qa/school/addressbok/ContactCreationTests.java
@@ -0,0 +1,15 @@
+package ru.ls.qa.school.addressbok;
+
+import org.junit.jupiter.api.Test;
+
+public class ContactCreationTests extends TestBase{
+    @Test
+    public void newTest() {
+
+        addNewContactPage();
+        fillContactForm("Имя", "Отчество", "Фамилия", "Прозвище", "Заголовок", "Компания", "Адрес", "+7 (999) 999-99-99", "+7 (888) 888-88-88", "+7 (777) 777-77-77", "+7 (666) 666-66-66", "email1@mail.ru", "email2@mail.ru", "email3@mail.ru", "homepage.ru", "1995", "2025", "Адрес2", "Дом", "Примечание");
+        submitNewContact();
+    }
+
+}
+
