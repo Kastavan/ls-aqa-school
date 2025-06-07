7ec970995dcd89809c590af00bb9d1582d1b2d07
Kastavan <kastavan4eg@gmail.com>
2025-06-07 13:55:15 +0300

    Рефакторинг тестов, создание цепочек событий, вынос селекторов в SelenieElement

--- addressbook-web-tests/src/test/java/ru/ls/qa/school/addressbok/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/ls/qa/school/addressbok/tests/GroupModificationTests.java
@@ -1,27 +1,26 @@
 package ru.ls.qa.school.addressbok.tests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.ls.qa.school.addressbok.model.GroupData;
 import ru.ls.qa.school.addressbok.pages.ContactsListPage;
-import ru.ls.qa.school.addressbok.pages.GroupPage;
-import ru.ls.qa.school.addressbok.pages.GroupsListPage;
 
 public class GroupModificationTests extends TestBase {
     ContactsListPage contactPage;
-    GroupsListPage groupsPage;
-    GroupPage groupEditPage;
+
+    @BeforeEach
+    public void precondition() {
+        contactPage = getPage.contacts();
+    }
 
     @Test
     public void testGroupModification() {
-        contactPage = getPage.contacts();
-        groupsPage = getPage.getGroupsListPage();
-        groupEditPage = getPage.getGroupPage();
 
-        contactPage.gotoGroups();
-        groupsPage.selectFirstGroup()
-                  .initGroupModification();
-        groupEditPage.editForm(new GroupData("test1", "test2", "test3"))
-                     .submitGroupModification()
-                     .returnToGroups();
+        contactPage.gotoGroups()
+                .selectFirstGroup()
+                .initGroupModification()
+                .editForm(new GroupData("test1", "test2", "test3"))
+                .submitGroupModification()
+                .returnToGroups();
     }
 }
